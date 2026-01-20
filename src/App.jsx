3e79da3bbb412670a75f6afdc11a26206e351dde
nishakrworkspace-pixel <nishakr.workspace@gmail.com>
2026-01-20 19:48:53 +0530

    Remove HomePage2 temporarily to fix Vercel build

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 
 import HomePage1Layout from "./components/HomePage1/HomePage1Layout";
-import HomePage2Layout from "./components/HomePage2/HomePage2Layout.jsx";
+//import HomePage2Layout from "./components/HomePage2/HomePage2Layout";
 import HomePage3Layout from "./components/HomePage3/HomePage3Layout";
 import BookGridLayout from "./components/BookGrid/BookGridLayout";
 
@@ -9,7 +9,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage1Layout />} />
-      <Route path="/menu" element={<HomePage2Layout />} />
+     
       <Route path="/home" element={<HomePage3Layout />} />
       <Route path="/books" element={<BookGridLayout />} />
     </Routes>
